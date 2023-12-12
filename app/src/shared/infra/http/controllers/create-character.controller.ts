ac76ec8d93492a4ca279d3471aa144c2d51482f7
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-12-12 21:59:19 +0100

    feat: controller create characters

--- app/src/shared/infra/http/controllers/create-character.controller.ts
+++ app/src/shared/infra/http/controllers/create-character.controller.ts
@@ -6,8 +6,17 @@ export class CreateCharacterController {
     async handle(request: Request, response: Response){
         const createUseCase = container.resolve(CreateCharactersUseCase)
 
-        await createUseCase.execute()
+        try {
+            await createUseCase.execute();
+            return response.status(200).json({ message: "Characters created successfully" });
+            
+        } catch (error) {
+            if (error instanceof Error && error.message === "No new characters to create") {
 
-        return response.status(200).json({message: "ok"})
+                return response.status(200).json({ message: "No new characters to create" });
+            }
+            // Outros erros podem ser tratados de acordo com as necessidades
+            return response.status(500).json({ error: "Internal Server Error" });
+        }
     }
 }
