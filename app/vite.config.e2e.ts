13580857208d973134d10abf470901c02f3e5d86
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-12-12 18:45:40 +0100

    chore: config para ajuste dos paths @

--- app/vite.config.e2e.ts
+++ app/vite.config.e2e.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig({
     test:{
@@ -7,5 +8,11 @@ export default defineConfig({
         globals: true,
         root: './'
     },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    
     plugins: [tsConfigPaths()]
 })
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig({
     test:{
         globals: true,
         root: './'
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        }
     },
     plugins: [tsConfigPaths()]
 })
